b1ee654f3ccbb4ab5aae2bff81dfb97d33a18502
Lassi Puurunen <puurunen.lassi@gmail.com>
2019-04-15 19:38:07 +0300

    Toimipiste-toiminnon toteutus aloitettu

--- VillagePeopleCottages/src/villagepeoplecottages/MainFXMLController.java
+++ VillagePeopleCottages/src/villagepeoplecottages/MainFXMLController.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
@@ -22,11 +23,10 @@ import javafx.scene.control.TitledPane;
  * 15.4.2019 Tiedosto luotu, Lassi Puurunen
  */
 public class MainFXMLController implements Initializable {
+
+    @FXML
+    private TableView<?> toimipisteetTableView;
     
-    @FXML
-    private TableView<Toimipiste> toimipisteetLista;
-    @FXML
-    private TitledPane toimipisteidenHallinta;
     
 
     
@@ -35,8 +35,13 @@ public class MainFXMLController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        
         
     }    
+
+    @FXML
+    private void lataaToimipisteet(Event event) {
+
+    }
     
 }
